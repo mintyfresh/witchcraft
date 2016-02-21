325a65f79db8925c991be2abd3108382d0f88611
Mihail-K <Mihail.K@fimu.tk>
2016-02-21 18:38:53 -0500

    Add documentation to constructor.

--- source/witchcraft/constructors.d
+++ source/witchcraft/constructors.d
@@ -6,10 +6,21 @@ import witchcraft;
 import std.string;
 import std.variant;
 
+/++
+ + Represents and grants access to a single constructor defined in a class.
+ ++/
 class Constructor : Member
 {
+    /++
+     + Invokes the constructor, passing in arguments as variant types.
+     + The result is returned as an Object type.
+     ++/
     abstract Object create(Variant[] arguments...) const;
 
+    /++
+     + Ditto, but accepts arguments of any type, and permits the result to also
+     + be cast to a type specified by template argument.
+     ++/
     T create(T : Object = Object, TList...)(TList arguments) const
     {
         auto values = new Variant[TList.length];
@@ -22,13 +33,22 @@ class Constructor : Member
         return cast(T) this.create(values);
     }
 
-    override string getName() const
+    /++
+     + Returns `__ctor`, which is the name of all constructors.
+     ++/
+    final override string getName() const
     {
         return "__ctor";
     }
 
+    /++
+     + Returns an array representing the constructor's parameter types.
+     ++/
     abstract const(TypeInfo)[] getParameterTypes() const;
 
+    /++
+     + Checks if the constructor accepts variable arguments.
+     ++/
     @property
     abstract bool isVarArgs() const;
 
