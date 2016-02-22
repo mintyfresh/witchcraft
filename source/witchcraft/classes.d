6343f98beeb2e6afaa1a25de1625daa6ae541eb7
Mihail-K <Mihail.K@fimu.tk>
2016-02-21 19:01:45 -0500

    Partial documentation for class.

--- source/witchcraft/classes.d
+++ source/witchcraft/classes.d
@@ -7,17 +7,47 @@ import std.algorithm;
 import std.array;
 import std.traits;
 
+/++
+ + Represents and grants access to a class's attributes, fields, methods,
+ + and constructors.
+ ++/
 abstract class Class : Member
 {
+    /++
+     + Creates a new instance of the class, provided it has a default or
+     + zero-argument constructor.
+     +
+     + Returns:
+     +   A new instance of the class.
+     ++/
     @property
     abstract Object create() const;
 
+    /++
+     + Ditto, but also casts the result to a type given by template parameter.
+     +
+     + Params:
+     +   T = An Object type to which the result is cast.
+     +
+     + Returns:
+     +   A new instance of the class.
+     ++/
     @property
     T create(T : Object)() const
     {
         return cast(T) this.create;
     }
 
+    /++
+     + Looks up and returns a constructor with a parameter list that exactly
+     + matches the given array of types.
+     +
+     + Params:
+     +   parameterTypes = A parameter list the constructor must exactly match.
+     +
+     + Returns:
+     +   The constructor object, or null if no such constructor exists.
+     ++/
     const(Constructor) getConstructor(TypeInfo[] parameterTypes...) const
     {
         foreach(constructor; getConstructors)
@@ -31,6 +61,15 @@ abstract class Class : Member
         return null;
     }
 
+    /++
+     + Ditto, but accepts types given by variadic template arguments.
+     +
+     + Params:
+     +   TList = A list of types the constructor must exactly match.
+     +
+     + Returns:
+     +   The constructor object, or null if no such constructor exists.
+     ++/
     const(Constructor) getConstructor(TList...)() const
     {
         auto types = new TypeInfo[TList.length];
@@ -43,8 +82,33 @@ abstract class Class : Member
         return this.getConstructor(types);
     }
 
+    /++
+     + Returns an array of all constructors defined by this class.
+     + This does not include any constructors inherited from a base class,
+     + nor the default constructor.
+     +
+     + If a class declares no constructors, this method will return an empty
+     + array. To construct an Object of such a type, the `create()` method
+     + defined by `Class` should be used.
+     +
+     + Returns:
+     +   And array of all constructors on the class.
+     +
+     + See_Also:
+     +   create
+     ++/
     abstract const(Constructor)[] getConstructors() const;
 
+    /++
+     + Looks up a field by name, searching this class, and each of its parent
+     + classes in turn.
+     +
+     + Params:
+     +   name = The name of the field.
+     +
+     + Returns:
+     +   The field object, or null if no such field exists.
+     ++/
     const(Field) getField(string name) const
     {
         auto field = getLocalField(name);
@@ -63,6 +127,14 @@ abstract class Class : Member
         }
     }
 
+    /++
+     + Returns an array of all fields defined by this class and classes that
+     + it inherits from. This only extends to parent classes that also use
+     + `Witchcraft`.
+     +
+     + Returns:
+     +   An array of all known field objects belonging to this class.
+     ++/
     const(Field)[] getFields() const
     {
         if(getParentClass !is null)
@@ -75,11 +147,32 @@ abstract class Class : Member
         }
     }
 
+    /++
+     + Returns an array of names of all fields defined by this class and
+     + classes that it inherits from. This only extends to parent classes that
+     + also use `Witchcraft`.
+     +
+     + This method is equivalent to mapping the result of `getFields()` to the
+     + names of the fields in the resulting array.
+     +
+     + Return:
+     +   An array of names of all known fields.
+     +
+     + See_Also:
+     +   getFields
+     ++/
     string[] getFieldNames() const
     {
         return getFields.map!"a.getName".array;
     }
 
+    /++
+     + Returns the fully-qualified name of the class, including the package and
+     + module name, and any types that might enclose it.
+     +
+     + Returns:
+     +   The fully-qualified name of this class.
+     ++/
     abstract string getFullName() const;
 
     abstract const(Field) getLocalField(string name) const;
@@ -200,9 +293,15 @@ abstract class Class : Member
         }
     }
 
+    /++
+     + Checks if this class is abstract.
+     ++/
     @property
     abstract bool isAbstract() const;
 
+    /++
+     + Checks if this class is final.
+     ++/
     @property
     abstract bool isFinal() const;
 }
